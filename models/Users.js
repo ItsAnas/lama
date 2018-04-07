2c197a0dba166b610e6eaaa533ae744d61a40115
Lucy Mambré <lucy.mambre@epitech.eu>
2018-04-07 19:31:01 +0200

    modify schema ads and users

--- models/Users.js
+++ models/Users.js
@@ -3,7 +3,7 @@ const UserSchema = mongoose.Schema({
     lastname: { type: String, required: true },
     name: { type: String, required: true },
     email: { type: String, required: true },
-    skills: { type: String, required: true },
+    skills: { type: Array, required: true },
 });
 
 module.exports = mongoose.model('User', UserSchema);
