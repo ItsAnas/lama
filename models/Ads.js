1352515565ce0297b36d5e53a43d65cf8ac6873c
Alexia Toulmet <Alex@MacBook-Pro-de-Alexia.local>
2018-04-07 21:31:37 +0200

    modif of models

--- models/Ads.js
+++ models/Ads.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 const AdSchema = mongoose.Schema({
     title: { type: String, required: true },
+    autor: { type: String, required: true },
+    profilepic: { type: String, required: true },
     description: { type: String, required: true },
     skills_need: { type: Array, required: true },
     nb_person: { type: Number, required: true },
