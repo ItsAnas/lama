7aea248110bb0fe32066106528170a6c6024b884
Mathilde Arconte <mathilde@m-arconte.fr>
2018-04-07 17:29:53 +0200

    Auth Facebook

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
     'facebookAuth' : {
-        'clientID'      : '1567701829995414',
-        'clientSecret'  : '1d65e71077ca0b07c35b2f419896864f',
+        'clientID'      : '1506494756332119',
+        'clientSecret'  : '194f00918b03172666d619ee06b9d4f9',
         'callbackURL'   : 'http://localhost:3000/fb/auth/facebook/callback',
     }
 }
