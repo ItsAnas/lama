fb2cb9f0bc187639d5443b57a20ce83bfe1988c6
Lucy Mambré <lucy.mambre@epitech.eu>
2018-04-08 06:48:01 +0200

    can sign in

--- routes/sign.js
+++ routes/sign.js
@@ -1,19 +1,41 @@
 var express = require('express');
 var router = express.Router();
 var connection = require('../database');
+var crypto = require('crypto');
 
 var Ads = require('../models/Ads');
+var Users = require('../models/Users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('signin');
+console.log("lol");
+    res.render('signin', { globalMessage: {} });
 });  
 
-router.get('/in', function(req, res, next) {
-    res.render('signin');
+router.get('/up', function(req, res, next) {
+    res.render('signup');
 });
 
-router.post('/api/form', function(req, res, next) {
+router.post('/api-connection', function(req, res) {
+    console.log(req.body);
+    var user = req.body;
+    Users.findOne({ email: user.email })
+    .then(data => {
+        var pass = crypto.createHash('md5').update(user.password).digest("hex");
+        if (data.password == pass) {
+            res.redirect('/home');
+        } else {
+            var messageData = {
+                message : "Bad password",
+                class : "failureMessage"
+            };
+            console.log("ici");
+            res.render('signin', { globalMessage: messageData });
+        }
+    });
+});
+
+router.post('/api-subscribe', function(req, res) {
 
 });
 
